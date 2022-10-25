df6e37ae54bd83684364d70818e2ad6e38668a01
Dang <ntdang_20th@student.agu.edu.vn>
2022-10-25 16:59:56 +0700

    fix double

--- livedata/thread.py
+++ livedata/thread.py
@@ -81,9 +81,8 @@ class StartSensor1(threading.Thread):
                     unit=Unit.objects.get(pk=3)
                 )
                 data = {'date': str(datetime.datetime.now()), 'UUID': 'd1e231bc-8bef-47f9-8383-70b51310a1d9', 'Touch': 'no touch detected', 'data': [acceleration_sensor.getJson(), gyroscope_sensor.getJson(), rotation_sensor.getJson()]}
-                print('----------------------------')
+
                 payload_json = json.dumps(data, indent=4)
-                print(payload_json)
                 headers = {
                     "accept": "text/plain, */*; q=0.01",
                     "accept-language": "en-US,en;q=0.9",
@@ -92,14 +91,9 @@ class StartSensor1(threading.Thread):
                 url = 'https://livedatawebsocket.herokuapp.com/sensor/post/'
 
                 x = requests.post(url, verify=True, data=payload_json, headers=headers)
-
-                print(x)
-                print('----------------------------')
-
                 time.sleep(1)
         except Exception as e:
             print(e)
-
 
 class StartSensor2(threading.Thread):
     def run(self):
@@ -124,18 +118,63 @@ class StartSensor2(threading.Thread):
                     rotationZ=random.uniform(-45, 45),
                     unit=Unit.objects.get(pk=3)
                 )
-                data = {'date': str(datetime.datetime.now()), 'UUID': 'bea44629-1aea-4404-b88c-7a0a0519b35d', 'Touch': 'no touch detected', 'data': [acceleration_sensor.getJson(), gyroscope_sensor.getJson(), rotation_sensor.getJson()]}
-                print(data)
+                data = {'date': str(datetime.datetime.now()),
+                        'UUID': 'bea44629-1aea-4404-b88c-7a0a0519b35d', 'Touch': 'no touch detected',
+                        'data': [acceleration_sensor.getJson(), gyroscope_sensor.getJson(), rotation_sensor.getJson()]}
 
+                payload_json = json.dumps(data, indent=4)
+                headers = {
+                    "accept": "text/plain, */*; q=0.01",
+                    "accept-language": "en-US,en;q=0.9",
+                    "content-type": "application/json; charset=UTF-8"
+                }
                 url = 'https://livedatawebsocket.herokuapp.com/sensor/post/'
 
-                x = requests.post(url, json=data)
-
-                print(x.text)
-
+                x = requests.post(url, verify=True, data=payload_json, headers=headers)
                 time.sleep(1)
         except Exception as e:
             print(e)
+
+            class StartSensor1(threading.Thread):
+                def run(self):
+                    try:
+                        print('Sensor 1 Start!')
+                        for i in range(60):
+                            acceleration_sensor = Acceleration.objects.create(
+                                valueX=random.uniform(-0.09, -0.01),
+                                valueY=random.uniform(0.01, 0.09),
+                                valueZ=random.uniform(1.01, 1.09),
+                                unit=Unit.objects.get(pk=1)
+                            )
+                            gyroscope_sensor = Gyroscope.objects.create(
+                                valueX=random.uniform(-1.8, -0.2),
+                                valueY=random.uniform(12.1, 13),
+                                valueZ=random.uniform(12.1, 13),
+                                unit=Unit.objects.get(pk=2)
+                            )
+                            rotation_sensor = Rotation.objects.create(
+                                rotationX=random.uniform(0.9, 16.9),
+                                rotationY=random.uniform(0.01, 4.9),
+                                rotationZ=random.uniform(-45, 45),
+                                unit=Unit.objects.get(pk=3)
+                            )
+                            data = {'date': str(datetime.datetime.now()),
+                                    'UUID': '68164eba-6d25-4d8b-982b-8f0803d21dff', 'Touch': 'touch detected',
+                                    'data': [acceleration_sensor.getJson(), gyroscope_sensor.getJson(),
+                                             rotation_sensor.getJson()]}
+
+                            payload_json = json.dumps(data, indent=4)
+                            headers = {
+                                "accept": "text/plain, */*; q=0.01",
+                                "accept-language": "en-US,en;q=0.9",
+                                "content-type": "application/json; charset=UTF-8"
+                            }
+                            url = 'https://livedatawebsocket.herokuapp.com/sensor/post/'
+
+                            x = requests.post(url, verify=True, data=payload_json, headers=headers)
+                            time.sleep(1)
+                    except Exception as e:
+                        print(e)
 
 class StartSensor3(threading.Thread):
     def run(self):
@@ -160,15 +199,18 @@ class StartSensor3(threading.Thread):
                     rotationZ=random.uniform(-45, 45),
                     unit=Unit.objects.get(pk=3)
                 )
-                data = {'date': str(datetime.datetime.now()), 'UUID': '68164eba-6d25-4d8b-982b-8f0803d21dff', 'Touch': 'no touch detected', 'data': [acceleration_sensor.getJson(), gyroscope_sensor.getJson(), rotation_sensor.getJson()]}
-                print(data)
+                data = {'date': str(datetime.datetime.now()), 'UUID': 'd1e231bc-8bef-47f9-8383-70b51310a1d9', 'Touch': 'no touch detected', 'data': [acceleration_sensor.getJson(), gyroscope_sensor.getJson(), rotation_sensor.getJson()]}
 
+                payload_json = json.dumps(data, indent=4)
+                headers = {
+                    "accept": "text/plain, */*; q=0.01",
+                    "accept-language": "en-US,en;q=0.9",
+                    "content-type": "application/json; charset=UTF-8"
+                }
                 url = 'https://livedatawebsocket.herokuapp.com/sensor/post/'
 
-                x = requests.post(url, json=data)
-
-                print(x.text)
-
+                x = requests.post(url, verify=True, data=payload_json, headers=headers)
                 time.sleep(1)
         except Exception as e:
             print(e)
+
